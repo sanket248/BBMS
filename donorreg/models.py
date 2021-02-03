f0bcb2155b519478c18faa3ea23e2f5be466aed2
sanket248 <gajerasanket123@gmail.com>
2021-02-03 17:05:31 +0530

    update donor table

--- donorreg/models.py
+++ donorreg/models.py
@@ -9,4 +9,14 @@ class Doner_details(models.Model):
     blood_group = models.CharField(max_length=3)
     address = models.CharField(max_length=30)
     city = models.CharField(max_length=10)
-    gender = models.CharField(max_length=6)
+    gender_choices=[
+        ('male',"Male"),
+        ("female","Female"),
+    ]
+    gender = models.CharField(
+        max_length=6, blank=True, null=True,
+        choices=gender_choices,
+        )
+    
+    def __str__(self):
+        return self.name
