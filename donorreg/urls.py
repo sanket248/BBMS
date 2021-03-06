376984ddaff07ee1349b1d65f7775ceafff052e1
sanket248 <gajerasanket123@gmail.com>
2021-03-06 18:38:32 +0530

    update donorreg

--- donorreg/urls.py
+++ donorreg/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
-from donorreg.views import dreg
+from donorreg.views import dreg, add, addsuccess
 from django.contrib.auth import views as auth_views 
 from django.conf.urls import url 
 
 urlpatterns = [ 
      url(r'donor-reg/$', dreg),
-] 
+     url(r'add-donor/$', add),
+     url(r'addsuccess/$', addsuccess),
+]
